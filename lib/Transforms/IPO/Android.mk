f0f3571b4514929d3da885bc5e3d7fb9924d95f7
youling257 <youling257@gmail.com>
2020-12-04 21:11:00 +0800

    android: add genrule "llvm-gen-omp-gen"

--- lib/Transforms/IPO/Android.mk
+++ lib/Transforms/IPO/Android.mk
@@ -53,6 +53,7 @@ LOCAL_MODULE_HOST_OS := darwin linux windows
 include $(LLVM11_HOST_BUILD_MK)
 include $(LLVM11_GEN_ATTRIBUTES_MK)
 include $(LLVM11_GEN_INTRINSICS_MK)
+include $(LLVM11_GEN_OMP_GEN_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
 
 # For the device
@@ -66,5 +67,6 @@ LOCAL_MODULE:= libLLVM11ipo
 include $(LLVM11_DEVICE_BUILD_MK)
 include $(LLVM11_GEN_ATTRIBUTES_MK)
 include $(LLVM11_GEN_INTRINSICS_MK)
+include $(LLVM11_GEN_OMP_GEN_MK)
 include $(BUILD_STATIC_LIBRARY)
 endif
--- llvm.mk
+++ llvm.mk
@@ -7,6 +7,7 @@ LLVM11_HOST_BUILD_MK := $(LLVM11_ROOT_PATH)/llvm-host-build.mk
 LLVM11_DEVICE_BUILD_MK := $(LLVM11_ROOT_PATH)/llvm-device-build.mk
 LLVM11_GEN_ATTRIBUTES_MK := $(LLVM11_ROOT_PATH)/llvm-gen-attributes.mk
 LLVM11_GEN_INTRINSICS_MK := $(LLVM11_ROOT_PATH)/llvm-gen-intrinsics.mk
+LLVM11_GEN_OMP_GEN_MK := $(LLVM11_ROOT_PATH)/llvm-gen-omp-gen.mk
 LLVM11_TBLGEN_RULES_MK := $(LLVM11_ROOT_PATH)/llvm-tblgen-rules.mk
 
 LLVM11_SUPPORTED_ARCH := arm arm64 mips mips64 x86 x86_64
