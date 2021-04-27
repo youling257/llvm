70f1496cd7ffa3846690e2999834c60a052f301e
youling257 <youling257@gmail.com>
2021-04-28 00:20:30 +0800

    android: [InstCombine] Move target-specific inst combining
    Porting of commit
    2a6c871 ("[InstCombine] Move target-specific inst combining")
    
    Fixes the following building error:
    
    module source path "external/llvm-project/llvm/lib/Transforms/InstCombine/InstCombineTables.td" does not exist

--- lib/Transforms/InstCombine/Android.mk
+++ lib/Transforms/InstCombine/Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-transforms_inst_combine_TBLGEN_TABLES12 := \
-  InstCombineTables.inc
-
 transforms_inst_combine_SRC_FILES := \
   InstructionCombining.cpp \
   InstCombineAddSub.cpp \
@@ -31,7 +28,6 @@ LOCAL_MODULE:= libLLVM12InstCombine
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
 include $(LLVM12_HOST_BUILD_MK)
-include $(LLVM12_TBLGEN_RULES_MK)
 include $(LLVM12_GEN_ATTRIBUTES_MK)
 include $(LLVM12_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
@@ -46,7 +42,6 @@ LOCAL_SRC_FILES := $(transforms_inst_combine_SRC_FILES)
 LOCAL_MODULE:= libLLVM12InstCombine
 
 include $(LLVM12_DEVICE_BUILD_MK)
-include $(LLVM12_TBLGEN_RULES_MK)
 include $(LLVM12_GEN_ATTRIBUTES_MK)
 include $(LLVM12_GEN_INTRINSICS_MK)
 include $(BUILD_STATIC_LIBRARY)
