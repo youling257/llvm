a43600e0b6997c39a8ed2c6fc2aa53b22598af67
youling257 <youling257@gmail.com>
2021-04-28 02:59:06 +0800

    android: build with -Wno-missing-braces -Wmissing-field-initializers -Wno-tautological-compare cppflag
    external/llvm12/lib/Target/X86/X86LowerAMXType.cpp:108:34: error: suggest braces around initialization of subobject [-Werror,-Wmissing-braces]
      std::array<Value *, 4> Args = {Row, Col, I8Ptr, Stride};
                                     ^~~~~~~~~~~~~~~~~~~~~~~
                                     {                      }
    external/llvm12/lib/Target/X86/X86LowerAMXType.cpp:136:34: error: suggest braces around initialization of subobject [-Werror,-Wmissing-braces]
      std::array<Value *, 5> Args = {Row, Col, I8Ptr, Stride, Tile};
                                     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                                     {                            }
    external/llvm12/lib/Target/X86/X86LowerAMXType.cpp:185:36: error: suggest braces around initialization of subobject [-Werror,-Wmissing-braces]
        std::array<Value *, 4> Args = {Row, Col, I8Ptr, Stride};
                                       ^~~~~~~~~~~~~~~~~~~~~~~
                                       {                      }
    external/llvm12/lib/Target/X86/X86LowerAMXType.cpp:203:36: error: suggest braces around initialization of subobject [-Werror,-Wmissing-braces]
        std::array<Value *, 5> Args = {Row, Col, I8Ptr, Stride, Src};
                                       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
                                       {                           }
    4 errors generated.
    make: *** [build/core/binary.mk:986: /1/1/out/target/product/android_x86/obj/STATIC_LIBRARIES/libLLVM12X86CodeGen_intermediates/X86LowerAMXType.o] Error 1
    make: *** Waiting for unfinished jobs....
    external/llvm12/lib/Target/X86/X86PreTileConfig.cpp:171:51: error: suggest braces around initialization of subobject [-Werror,-Wmissing-braces]
          std::array<MachineOperand *, 2> ShapeMOs = {Shape.getRow(),
                                                      ^~~~~~~~~~~~~~~
    1 error generated.
    make: *** [build/core/binary.mk:986: /1/1/out/target/product/android_x86/obj/STATIC_LIBRARIES/libLLVM12X86CodeGen_intermediates/X86PreTileConfig.o] Error 1
    
    In file included from external/llvm12/lib/Target/AMDGPU/AMDGPUSubtarget.cpp:40:
    /1/1/out/target/product/android_x86/obj/STATIC_LIBRARIES/libLLVM12AMDGPUCodeGen_intermediates/AMDGPUGenSubtargetInfo.inc:762:64: error: comparison of unsigned expression < 0 is always false [-Werror,-Wtautological-compare]
      if (Bits[AMDGPU::FeatureLocalMemorySize0] && LocalMemorySize < 0) LocalMemorySize = 0;
                                                   ~~~~~~~~~~~~~~~ ^ ~
    In file included from external/llvm12/lib/Target/AMDGPU/AMDGPUSubtarget.cpp:44:
    /1/1/out/target/product/android_x86/obj/STATIC_LIBRARIES/libLLVM12AMDGPUCodeGen_intermediates/R600GenSubtargetInfo.inc:282:62: error: comparison of unsigned expression < 0 is always false [-Werror,-Wtautological-compare]
      if (Bits[R600::FeatureLocalMemorySize0] && LocalMemorySize < 0) LocalMemorySize = 0;
                                                 ~~~~~~~~~~~~~~~ ^ ~
    target  C++: libLLVM12AMDGPUCodeGen <= external/llvm12/lib/Target/AMDGPU/AMDILCFGStructurizer.cpp
    2 errors generated.
    make: *** [build/core/binary.mk:986: /1/1/out/target/product/android_x86/obj/STATIC_LIBRARIES/libLLVM12AMDGPUCodeGen_intermediates/AMDGPUSubtarget.o] Error 1

--- llvm-host-build.mk
+++ llvm-host-build.mk
@@ -56,6 +56,8 @@ LOCAL_CPPFLAGS :=	\
 	-Wno-sign-compare	\
 	-Wno-sign-promo         \
 	-Wno-unused-variable	\
+	-Wno-missing-braces -Wmissing-field-initializers \
+	-Wno-tautological-compare \
 	-std=c++14
 
 LOCAL_CPPFLAGS_linux := \
