32586ba3da54bc9324a7140f0876701c7979a5d1
youling257 <youling257@gmail.com>
2021-04-28 00:34:19 +0800

    android: [VE] Support vld intrinsics
    Porting of commit
    410626c ("[VE] Support vld intrinsics")
    
    Fixes the following building error:
    
    external/llvm-project/llvm/lib/IR/Function.cpp:46:10: fatal error: 'llvm/IR/IntrinsicsVE.h' file not found
             ^~~~~~~~~~~~~~~~~~~~~~~~
    1 error generated.

--- llvm-gen-intrinsics.mk
+++ llvm-gen-intrinsics.mk
@@ -165,3 +165,13 @@ ifeq ($(LOCAL_IS_HOST_MODULE),true)
 else
 	$(call transform-device-td-to-out12,intrinsic-enums -intrinsic-prefix=xcore)
 endif
+
+GENFILE := $(addprefix $(call local-generated-sources-dir)/llvm/IR/,IntrinsicsVE.h)
+LOCAL_GENERATED_SOURCES += $(GENFILE)
+$(GENFILE): TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
+$(GENFILE): $(INTRINSICTD12) $(INTRINSICTD12S) | $(LLVM12_TBLGEN)
+ifeq ($(LOCAL_IS_HOST_MODULE),true)
+	$(call transform-host-td-to-out12,intrinsic-enums -intrinsic-prefix=ve)
+else
+	$(call transform-device-td-to-out12,intrinsic-enums -intrinsic-prefix=ve)
+endif
